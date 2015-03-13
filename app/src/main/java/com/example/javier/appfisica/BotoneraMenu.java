846fa540693bbe7e85649f2afa7ba84cddc9e421
javier <javiferheb91@hotmail.com>
2015-03-13 19:14:09 +0100

    String

--- app/src/main/java/com/example/javier/appfisica/BotoneraMenu.java
+++ app/src/main/java/com/example/javier/appfisica/BotoneraMenu.java
@@ -1,39 +1,24 @@
 package com.example.javier.appfisica;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    private final int DURACION_SPLASH = 3000;
+public class BotoneraMenu extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        new Handler().postDelayed(new Runnable(){
-            public void run(){
-                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
-                Intent intent = new Intent(MainActivity.this, BotoneraMenu.class);
-
-                startActivity(intent);
-                finish();
-            };
-    }, DURACION_SPLASH);
-   }
+        setContentView(R.layout.activity_botonera_menu);
+    }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_botonera_menu, menu);
         return true;
     }
 
