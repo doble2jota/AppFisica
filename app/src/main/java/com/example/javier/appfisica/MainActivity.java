4d09d2e1055c5a971e2983a6d8070819d1e49d53
javier <javiferheb91@hotmail.com>
2015-03-14 17:34:20 +0100

    Sabado

--- app/src/main/java/com/example/javier/appfisica/MainActivity.java
+++ app/src/main/java/com/example/javier/appfisica/MainActivity.java
@@ -15,7 +15,11 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+
+
 
 
         new Handler().postDelayed(new Runnable(){
