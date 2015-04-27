3d69127ed5f84f4cee5cee474b835ec644141f66
javier <javiferheb91@hotmail.com>
2015-04-27 14:50:29 +0200

    la chinada

--- app/src/main/java/com/example/javier/appfisica/CalculosActivity.java
+++ app/src/main/java/com/example/javier/appfisica/CalculosActivity.java
@@ -1,5 +1,7 @@
 package com.example.javier.appfisica;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -462,7 +464,6 @@ public class CalculosActivity extends ActionBarActivity {
 */
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -478,10 +479,31 @@ public class CalculosActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.btInfo) {
+            acerca();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Función para mostrar la opción ACERCA DE de la aplicación.
+     */
+    public void acerca() {
+        AlertDialog.Builder dialogoAcerca = new AlertDialog.Builder(this);
+
+        dialogoAcerca.setTitle(R.string.acerca);
+        dialogoAcerca.setMessage(R.string.mensajeAcerca);
+        dialogoAcerca.setCancelable(false);
+        dialogoAcerca.setNegativeButton(R.string.aceptar, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        dialogoAcerca.show();
+    } //Fin acerca()
 }
